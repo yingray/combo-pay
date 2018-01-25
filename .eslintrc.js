f1fed78edcdb3aa752408101a3634f4090bf3f94
Yingray Lu <lu.yingray@gmail.com>
2018-01-25 23:18:02 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
+    "semi": 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
